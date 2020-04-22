f24831b857afaf78e09d6eb3538ac020e13363b1
littlebaba <453984016@qq.com>
2020-04-22 21:06:15 +0800

    empty shelf

---
+++ src/main/java/com/lh/booktrading/BooktradingApplication.java
@@ -0,0 +1,13 @@
+package com.lh.booktrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooktradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lh/booktrading/BooktradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.booktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
