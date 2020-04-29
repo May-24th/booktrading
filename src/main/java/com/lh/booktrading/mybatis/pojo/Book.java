ac6db113e060663182ef95b98d49134661ee43e3
May-24th <littlebaba0304@163.com>
2020-04-29 23:59:22 +0800

    pojo defined

---
+++ src/main/java/com/lh/booktrading/mybatis/pojo/Book.java
@@ -0,0 +1,19 @@
+package com.lh.booktrading.mybatis.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private Long id;
+    private String name;
+    private float price;
+    private String ISBN;
+    private String publisher;
+    private Category category;
+    private User owner;
+    private BookImage bookImage;
+}
---
+++ src/main/java/com/lh/booktrading/mybatis/pojo/BookImage.java
@@ -0,0 +1,14 @@
+package com.lh.booktrading.mybatis.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookImage {
+    private Long id;
+    private Book book;
+    private String type;
+}
